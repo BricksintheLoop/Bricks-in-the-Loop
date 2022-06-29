0a84c99d5e9b182935d5f405e6994c52ac62993a
BricksintheLoop <106754185+BricksintheLoop@users.noreply.github.com>
2022-06-29 13:38:08 -0400

    code commented

--- BIL_CameraVideo.py
+++ BIL_CameraVideo.py
@@ -1,7 +1,10 @@
-import cv2
-#useing the vdieo over IP network (
-cam = cv2.VideoCapture('rtsp://admin:BIL_2022@192.168.30.128')
 
+"""BIL Camera feed """
+import cv2 				# IP camera can be access in opencv by streaming URL of
+
+# Using the video over IP network
+cam = cv2.VideoCapture('rtsp://admin:BIL_2022@192.168.30.128')
+					# connecting to camera username (admin) & password (BIL_2022) @ camera IP address
 while True:
 	ret, frame = cam.read()
 	cv2.imshow('frame',frame)
